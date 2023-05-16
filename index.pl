bd30ba02938f29866cf8a335b80b74e82dcb8b09
solomon monotilla <solomon.monotilla@gmail.com>
2023-05-16 19:42:32 +0800

    remove json support

--- index.pl
+++ index.pl
@@ -1,15 +1,20 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use JSON;
 
-my $filename = 'data.json';
-open(my $fh, '<', $filename) or die "Could not open file '$filename' $!";
-my $json_text = do { local $/; <$fh> };
-my $json = JSON->new;
-my $data = $json->decode($json_text);
+my @data = (
+    {
+        "name" => "apples",
+        "baskets" => [10,20,30]
+    },
+    {
+        "name" => "bananas",
+        "baskets" => [5,20,10,10]
+    }
+);
 
-foreach my $item (@$data) {
+
+foreach my $item (@data) {
     my $name = $item->{name};
     my $baskets = $item->{baskets};
     my $produce_count = 0;
@@ -18,9 +23,6 @@ foreach my $item (@$data) {
         $produce_count += $count;
         $baskets_count++;
     }
-# 60 apples in 3 baskets
-# 45 bananas in 4 baskets
     print "$produce_count  $name in $baskets_count baskets\n";
-}
 
- 
+}
